11c7c6dfbe815c9495610a747761e2fbaca7773b
OgutuJoseph <onyango.joseph@yahoo.com>
2023-05-25 12:42:04 +0300

    Add reset password fields migration

--- angular-auth-api/Migrations/20230525094022_addResetPasswordFields.Designer.cs
+++ angular-auth-api/Migrations/20230525094022_addResetPasswordFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using angular_auth_api.Context;
 
@@ -11,9 +12,11 @@ using angular_auth_api.Context;
 namespace angular_auth_api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230525094022_addResetPasswordFields")]
+    partial class addResetPasswordFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
